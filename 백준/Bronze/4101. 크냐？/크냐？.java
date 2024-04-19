af7c38ddb15c3d056e555756eae295d3bb79e3e5
Hyu-Jo <160295380+Hue-Jo@users.noreply.github.com>
2024-04-19 15:28:55 +0900

    [Bronze V] Title: 크냐?, Time: 132 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            while(true) {
+                String input = br.readLine();
+                String[] inputs = input.split(" ");
+                int first = Integer.parseInt(inputs[0]);
+                int second = Integer.parseInt(inputs[1]);
+
+
+                if (first == 0 && second== 0) {
+                    break;
+                }
+                if (first > second) {
+                    System.out.println("Yes");
+                } else {
+                    System.out.println("No");
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+       }
+    }
