a2bf13e1b01f0efd2c46a6b482b20f9b0ec511b6
Hyu-Jo <160295380+Hue-Jo@users.noreply.github.com>
2024-04-27 00:55:28 +0900

    Update 특수문자 출력하기.java

--- 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -1,5 +1,15 @@
 public class Solution {
     public static void main(String[] args) {
         System.out.print("!@#$%^&*(" + "\\" + "\'" + "\"" + "<>?:;");
+    }
 }
-}
+
+/*
+이스케이프 문자 = 특수문자 중에서 이렇게 써야하는 애들이 있음 
+
+"를 출력하려면: \"
+'를 출력하려면: \'
+\를 출력하려면: \\
+줄 바꿈을 출력하려면: \n
+
+*/
