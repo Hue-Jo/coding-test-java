2276e2e37892d97106f3d8bcce454de5571381e9
Hue-Jo <160295380+Hue-Jo@users.noreply.github.com>
2025-03-19 23:16:42 +0900

    [level 0] Title: 정수 찾기, Time: 4.64 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,10 +1,8 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] num_list, int n) {
-        for (int num : num_list){
-            if (num == n){
-                return 1;
-            }
-        }
-        return 0;
+        return Arrays.stream(num_list)
+            .anyMatch(num -> num == n) ? 1 : 0;
     }
 }
