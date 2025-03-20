8c7a105717d0eac8dc37ec22396e70ab566bb2ce
Hue-Jo <160295380+Hue-Jo@users.noreply.github.com>
2025-03-20 22:48:38 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.90 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,16 +1,12 @@
 class Solution {
     public int solution(int a, int b) {
         
-        String str1 = String.valueOf(a) + String.valueOf(b);
-        String str2 = String.valueOf(b) + String.valueOf(a);
+        String str1 = String.valueOf(a);
+        String str2 = String.valueOf(b);
         
-        int x = Integer.valueOf(str1);
-        int y = Integer.valueOf(str2);
+        int x = Integer.valueOf(str1 + str2);
+        int y = Integer.valueOf(str2 + str1);
         
-        if (y > x) {
-            return y;
-        } else {
-            return x;
-        }
+        return Math.max(x, y);
     }
 }
