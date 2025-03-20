0dc95535023529d8406b1633a76b12e307e0783d
Hue-Jo <160295380+Hue-Jo@users.noreply.github.com>
2025-03-20 22:29:26 +0900

    [level 0] Title: 문자열 섞기, Time: 0.05 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,25 +1,13 @@
-import java.util.LinkedList;
-import java.util.Queue;
 
 class Solution {
     public String solution(String str1, String str2) {
         
-        Queue<Character> q1 = new LinkedList<>();
-        Queue<Character> q2 = new LinkedList<>();
-        StringBuilder sb = new StringBuilder();
+       StringBuilder sb = new StringBuilder();
         
-        for(char c : str1.toCharArray()) {
-            q1.offer(c);
+        for(int i = 0; i < str1.length(); i++){
+            sb.append(str1.charAt(i));
+            sb.append(str2.charAt(i));
         }
-        
-        for(char c : str2.toCharArray()) {
-            q2.offer(c);
-        }
-        
-        while(!q1.isEmpty() && !q2.isEmpty()){
-            sb.append(q1.poll());
-            sb.append(q2.poll());
-        }
-        return sb.toString();
+        return sb.toString(); 
     }
 }
